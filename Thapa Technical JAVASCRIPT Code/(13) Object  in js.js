756c1b06eb2ef327038e3b66280ab0fb5af6fe40
Vinay Singh <vinaysinghcktd01@gmail.com>
2024-09-28 09:36:58 +0530

    Destructuring concept, few other function added

--- Thapa Technical JAVASCRIPT Code/(13) Object  in js.js
+++ Thapa Technical JAVASCRIPT Code/(13) Object  in js.js
@@ -1,6 +1,6 @@
 
 // Object in java script are stored in the form of key value pair
-// In object key can be string or varaiable name .
+// In object key can be string or variable name .
 
 
 // 1. Creation of Object - 2 ways
@@ -27,14 +27,14 @@ console.log(typeof(stud));  // object
 
 
  // 2. Accessing & creation of the properties of the Object - 2 ways to do that
-    // (a ) using dot Operator - we can only access the valid identifire. The property name only start with characters. And it also won't work when passing the object as argument in function.
+    // (a ) using dot Operator - we can only access the valid identifier. The property name only start with characters. And it also won't work when passing the object as argument in function.
         console.log(stud.age); // 22
 
         stud.adress = " Bhinod wali raod gali 4"; // Adding the property in in Object
 
         console.log(stud); // gives the whole object
 
-        // (B) USing [] -it can be used anywehere. It access the property by 
+        // (B) USing [] -it can be used anywhere. It access the property by [] 
         console.log(stud2['age']);  // 24
         
         stud2["232"] = "+9123223232"; // Adding the property in Object
@@ -51,14 +51,14 @@ console.log(typeof(stud));  // object
 
 /* 4.There are two things that are very important in objects - 
     ● Objects are stored in heap. 
-    ● Objects are reference - means in the above example the stud & stud2 are storing the adress of the objects. using that refernce adress we are accessing the property.
+    ● Objects are reference - means in the above example the stud & stud2 are storing the address of the objects. using that reference address we are accessing the property.
 */
 
  var item1 = {
     name : "Steel"
  }
 
- var item2  = item1 ; // now item2 start reffering to the same object . It will not copy the object.
+ var item2  = item1 ; // now item2 start referring to the same object . It will not copy the object.
 
 console.log(item1 === item2 ); // returns true
 
@@ -84,12 +84,42 @@ console.log(item3.address.Block); // 232 Gali
 
 // 7. Object Dynamic property- Update in ECMA Script.
 
-var _name = "Nominee_name"
+var name1 = "Nominee_name"
 var _age = "Nominee_age";
     var item4 ={
-    [_name]: "vinod Thapa", // here it act as value
+    [name1]: "vinod Thapa", // here it act as value
     [_age] : 32
     }
 console.log(item4);
-console.log(item4[_name]);
+console.log(item4[name1]);
+
+// 8. storing the Symbol data type value in Object - important for interview
+
+var Sym1 = Symbol("key1")
+var Obj3 = {
+    name3 : "Pat",
+    [Sym1] : "Value 1",
+    address : "Gal no 3232 / 10"
+
+}
+
+console.log(Obj3[Sym1]);
+
+// 9. Freeze () - Method of object which freeze the value from changing.
+
+Object.freeze(Obj3)
+Obj3["name3"] = "Simon"
+console.log(Obj3);
+
+// 10. Key and Values - These method used to store the all key and values respectively.
+
+console.log(Object.keys(Obj3))  //[ 'name3', 'address' ]
+console.log(Object.values(Obj3)) // [ 'Pat', 'Gal no 3232 / 10' ]
+
+
+// 11. Destructuring of Object 
+
+const {name3, address}  = Obj3
+console.log(`The name is ${name3}, and the address is ${address}`)
+
 
